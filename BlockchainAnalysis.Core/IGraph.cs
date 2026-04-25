bce49bb7f7aef24a4694572718ea5963ed881186
TheQayu <mehmetoguzhan2504@gmail.com>
2026-04-26 01:28:13 +0300

    Commit 1: Proje iskeletinin olusturulmasi ve temel klasor yapisinin ayarlanmasi.

---
+++ BlockchainAnalysis.Core/IGraph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BlockchainAnalysis.Models;
+
+namespace BlockchainAnalysis.Core
+{
+    public interface IGraph
+    {
+        void AddVertex(Wallet wallet);
+        void AddEdge(Transaction transaction);
+    }
+}
